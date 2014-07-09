9e14cc47a515abf69add80f212ab9ff348051207
ofedorov <aleks.n.fedorov@gmail.com>
2014-07-08 18:09:36 -0700

    Move test context initialization to object construction phase

--- core/src/main/java/com/scejtesting/core/context/TestContextService.java
+++ core/src/main/java/com/scejtesting/core/context/TestContextService.java
@@ -3,6 +3,8 @@ package com.scejtesting.core.context;
 import com.scejtesting.core.config.Test;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * User: Fedorovaleks
@@ -11,6 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class TestContextService {
 
     private static final ConcurrentHashMap<Integer, TestContext> contexts = new ConcurrentHashMap<Integer, TestContext>();
+    private static Lock contextLock = new ReentrantLock(false);
 
     public TestContext getCurrentTestContext() {
         return TestContext.getInstance();
