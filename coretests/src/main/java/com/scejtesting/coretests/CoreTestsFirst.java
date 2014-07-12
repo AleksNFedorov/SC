c823c0b83ea9a1abb5e3d8fc5a44361e8138e9fd
aleks <aleks.n.fedorov@gmail.com>
2014-07-11 23:00:42 -0700

    Initial test set

--- coretests/src/main/java/com/scejtesting/coretests/CoreTestsFirst.java
+++ coretests/src/main/java/com/scejtesting/coretests/CoreTestsFirst.java
@@ -1,8 +1,18 @@
 package com.scejtesting.coretests;
 
+import com.scejtesting.core.concordion.extension.ScejCoreExtensions;
+import org.concordion.api.extension.Extensions;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ofedorov on 7/11/14.
  */
+@RunWith(ConcordionRunner.class)
+@Extensions(value = ScejCoreExtensions.class)
 public class CoreTestsFirst {
 
     public String getSame(String content) {
@@ -19,6 +29,14 @@ public class CoreTestsFirst {
 
     public boolean getFalse() {
         return Boolean.FALSE;
+    }
+
+    public List<Integer> getList(Integer count) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < count; ++i) {
+            list.add(i);
+        }
+        return list;
     }
 
     public void throwException() throws Exception {
